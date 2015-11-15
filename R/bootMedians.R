389b4a87a34e5b51f8acfd83512d04ebb9708c37
steveped <stephen.pederson.au@gmail.com>
2015-11-15 20:39:01 +1030

    Added maxP argument to bootMedians function

--- R/bootMedians.R
+++ R/bootMedians.R
@@ -14,6 +14,8 @@
 #' @param ... Passed to the function \code{mean} internally
 #' @param na.rm \code{logical}. Also passed internally to the function \code{mean}
 #' @param replace \code{logical}. Should the bootstrap use sampling with replacement (\code{replace = TRUE}) or without
+#' @param maxP The maximum probability (weight) allowed for an individual gene in the reference set. 
+#' Defaults to \code{1/nGenes}
 #'
 #' @details
 #'This function breaks the supplied \code{data.frame} into two sets of test IDs & reference IDs.
@@ -31,6 +33,11 @@
 #'This allows direct comparison of these values as they will be drawn from similar
 #'distributions based on the binning variable used.
 #'
+#'If any genes have a probability of being resampled > \code{maxP} they may exert undue influence on the results.
+#'If any are found the process will stop to allow removal of this grouping.
+#'Alternatively, the value for \code{maxP} can be reset up to a maximum of 1, 
+#'which would represent maximum permissability.
+#'
 #' @author Steve Pederson
 #'
 #' @return 
@@ -40,6 +47,7 @@
 #'  \item \code{$p} The proportion of sampled differences which are > 0
 #'  \item \code{$nGenes} The number of genes sampled at each bootstrap iteration
 #'  \item \code{$nBoot} The number of bootstrap iterations
+#'  \item \code{sampleSizes} A named numeric vector with the sample sizes for each dataset
 #'  \item \code{$testBins} The distributions of genes amongst the binning variable in the set of test IDs
 #'  \item \code{$refBins} The distributions of genes amongst the binning variable in the set of reference IDs.
 #'  The final column represents the sampling probability for each individual gene in the corresponding bin
@@ -50,13 +58,14 @@
 bootMedians <- function(data, testIds, refIds, 
                         idCol = 1L, binCol = "lengthBin", valCols = "TPM",
                         nGenes = 1000L, nBoot = 100L, 
-                        minGenes = 200L, ..., na.rm=TRUE, replace = TRUE){
+                        minGenes = 200L, ..., na.rm=TRUE, replace = TRUE, maxP){
   
   stopifnot(is.data.frame(data))
   stopifnot(is.character(testIds))
   stopifnot(is.character(refIds))
   stopifnot(is.numeric(nGenes))  
   stopifnot(is.numeric(nBoot))
+  stopifnot(is.logical(na.rm), is.logical(replace))
   
   nGenes <- as.integer(nGenes)
   nBoot <-as.integer(nBoot)
@@ -120,26 +129,44 @@ bootMedians <- function(data, testIds, refIds,
                                 " respectively")
   }
   
+  if(missing(maxP)) {
+    maxP <- 1 / nGenes
+  }
+  else {
+    stopifnot(is.numeric(maxP), maxP <= 1) # Ensure a numeric value <= 1
+  }
+  
+  # Now just get the key information from the supplied data
   data <- select(data, ID, bins, one_of(valCols))
   
-  # Set up the test data_frame
+  # Set up the test data frame
   testData <- filter(data, ID %in% testIds)
   testBins <- summarise(group_by(testData, bins), count = n())
   testBins <- mutate(ungroup(testBins), p = count / sum(count))
   
-  # Set up the reference data_frame
+  # Set up the reference data frame
   refData <- filter(data, ID %in% refIds)
   refBins <- summarise(group_by(refData, bins), count = n())
   refBins <- mutate(ungroup(refBins), 
                     p = count / sum(count), 
                     effP = testBins$p / count) # This is the probability/gene
+  
   # Assign bin weights for the reference dataset to ensure the matching sampling distributions
   wRef <- refBins$effP[match(refData$bins, refBins$bin)]
+  refBins <- mutate(refBins, effP = effP / sum(wRef)) # Rescale effP to ensure a sum to 1
   
+  # Check for any points with potentially excessive influence.
+  # This defaults to checking for points with a probability of inclusion > 1 for each iteration
+  if (max(refBins$effP) > maxP) stop("Genes from one or more groups have probabilities of being resampled >",
+                                     maxP, "\n", refBins)
+  
+  # The sampling function
   sampleBoth <- function(testData, refData, nGenes, testBins, replace, wRef){
     
+    # Take the samples
     testSamp <- sample_n(testData, nGenes, replace)
     refSamp <- sample_n(refData, nGenes, replace, weight = wRef)
+    # Find the median expression value & find the difference
     testMeds <- summarise_each(testSamp, funs(median), one_of(valCols))
     refMeds <- summarise_each(refSamp, funs(median), one_of(valCols))
     meds <- testMeds - refMeds
@@ -147,16 +174,20 @@ bootMedians <- function(data, testIds, refIds,
     
   }
   
+  # Now run the tilted bootstrap
   out <- replicate(nBoot, 
                    sampleBoth(testData, refData, nGenes, testBins, replace, wRef), 
                    simplify = FALSE)
   out <- bind_rows(out)
   p <- vapply(out,function(x){mean(x > 0, na.rm =na.rm, ...)}, numeric(1))
   
+  # And the results
   return(list(samples = out,
               p = p,
               nGenes = nGenes,
               nBoot = nBoot,
+              sampleSizes = c(test = length(testIds), 
+                              ref = length(refIds)),
               testBins = testBins,
               refBins = refBins))
   
